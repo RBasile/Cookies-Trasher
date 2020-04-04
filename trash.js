8eeb92b1266575d3060a9356c74337aa7aa7f923
RBasile <basile.rouault@hotmail.fr>
2020-04-04 23:36:12 +0200

    * chg: better css for the button * add: reloadPage function

--- trash.js
+++ trash.js
@@ -1,12 +1,19 @@
-function removeAllcookie(cookies,tab,btn) {
+function removeAllcookie(cookies,tab,btn,text) {
     if (cookies.length > 0) {
       for (let cookie of cookies) {
         let removing = browser.cookies.remove({name: cookie.name,url: tab.url,storeId: tab.cookieStoreId})
       }
-      btn.innerHTML = "Deleted !!";
+      text.innerHTML = "Cookies are Trashed"
+      btn.innerHTML = "Reload ?";
+      btn.onclick = function() {reloadPage(tab)};
     } else {
       btn.innerHTML = "No cookie to delete";
     }
+}
+function reloadPage(tab) {
+  console.log("reloadé")
+  browser.tabs.reload();
+  window.close()
 }
 function detectcookiesForTab(tabs) {
   //get the first tab object in the array
@@ -20,7 +27,7 @@ function detectcookiesForTab(tabs) {
       var btn = document.createElement("BUTTON");
       btn.innerHTML = "Trash it";
       activeTabUrl.parentNode.appendChild(btn);
-      btn.onclick = function() {removeAllcookie(cookies,tab,btn)};
+      btn.onclick = function() {removeAllcookie(cookies,tab,btn,activeTabUrl)};
     } else {
       var text = document.createTextNode("No Cookies at: "+tab.title);
     }
